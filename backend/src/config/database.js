f812694291c68aa3a6b70bc71c1b21335411af9e
Renan Lopes <renanlopescoder@gmail.com>
2017-07-26 15:25:02 -0300

    feature: package.json to start the app in heroku server
    feature: Configuring the database on mLab Heroku addOn

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-module.exports = mongoose.connect('mongodb://localhost/todo', { useMongoClient: true } )
+module.exports = mongoose.connect('mongodb://renanlopes:openpassword@ds125623.mlab.com:25623/heroku_ck985tw6', { useMongoClient: true } )
